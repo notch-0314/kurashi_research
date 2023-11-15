0a115aaa98b12f5a47e683ddd680d745cfdfbb4d
notch-0314 <r.yosuke.inoue0314@gmail.com>
2023-11-15 14:13:25 +0900

    st.write

--- main.py
+++ main.py
@@ -507,9 +507,6 @@ def start_button_clicked(input_email_or_phone, input_password):
     # StreamlitでHTMLを表示
     st.write(text)
     
-    
-    st.title('ヘッダー表示')
-    
     try:
         wait_for_element_clickable(browser, By.ID, "masthead-container")
     except StaleElementReferenceException:  # エラーが発生した場合、要素を再取得して操作を試みる
